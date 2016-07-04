cd659098c4515d82b8248113d8e115331631451a
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-07-04 07:58:42 +0200

    Updated hearder

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -6,7 +6,7 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/01 08:09:54 by daviwel           #+#    #+#             */
-/*   Updated: 2016/07/01 13:24:52 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/07/04 07:58:23 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <SDL.h>
+# include "mlx.h"
+# define WIN_X 640
+# define WIN_Y 420
+
+typedef struct	s_vector
+{
+	float		x;
+	float		y;
+	float		z;
+}				t_vector;
+
+typedef struct	s_env
+{
+	void		*mlx;
+	void		*win;
+}				t_env;
 
 #endif
