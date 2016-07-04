f30e50e05f0cb850eec1acf3b56944bb4cd76b3b
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-07-04 11:26:57 +0200

    a

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -6,7 +6,7 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/01 08:09:54 by daviwel           #+#    #+#             */
-/*   Updated: 2016/07/04 10:58:34 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/07/04 11:26:22 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <math.h>
 # include <mlx.h>
 # include "vector.h"
+# include "colour.h"
 # include "../libft/libft.h"
 # define WIN_X 640
 # define WIN_Y 420
@@ -27,5 +28,7 @@ typedef struct	s_env
 	void		*mlx;
 	void		*win;
 }				t_env;
+
+void	mlx_image_put_pixel(void *mlx, t_img *i, t_coordint p, t_col *c);
 
 #endif
